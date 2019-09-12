4145c1c149c33fc861ca24e5e4ba0e6d3ff11616
David Piper <black.god1930@gmail.com>
2019-09-12 17:39:06 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.90c2d43453294d5728637c76fa2c3101.js"
+  "/portfolio/precache-manifest.e71043773683b438621ca71d39905d61.js"
 );
 
 self.addEventListener('message', (event) => {
